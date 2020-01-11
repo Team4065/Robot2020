95093645801c925df13ee54397bb1a308c8e4409
KueZ <KueZie@github.com>
2020-01-11 11:34:38 -0500

    Fixed some formatting issues; organizational.

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -2,3 +2,9 @@
 
 // This method will be called once per scheduler run
 void Shooter::Periodic() {}
+
+Shooter& Shooter::GetInstance()
+{
+    static Shooter instance;  // Guaranteed to be destroyed.
+    return instance;
+}
