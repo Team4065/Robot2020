e13af2cf98cc5e7a181a7007134b2d22243916f8
Collin <collin_stebbins@outlook.com>
2020-01-11 11:38:26 -0500

    Added On and Off functions to shooter subsystem

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -2,3 +2,13 @@
 
 // This method will be called once per scheduler run
 void Shooter::Periodic() {}
+
+void Shooter::On(){
+    left.Set(0.1);
+    //right.Set(0); keep off until final testing to prevent the motor from fighting
+}
+
+void Shooter::Off(){
+    left.Set(0);
+    //right.Set(0);
+}
