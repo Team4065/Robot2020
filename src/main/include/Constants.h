a682a07fcfa797db167c727b712b0a899c7876d0
KueZ <KueZie@github.com>
2020-01-11 11:49:06 -0500

    Added constants

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -1,18 +1,20 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
 #pragma once
 
-/**
- * The Constants header provides a convenient place for teams to hold robot-wide
- * numerical or boolean constants.  This should not be used for any other
- * purpose.
- *
- * It is generally a good idea to place constants into subsystem- or
- * command-specific namespaces within this header, which can then be used where
- * they are needed.
- */
+namespace constants
+{
+    namespace shooter
+    {
+        constexpr int kLeftMotorPort = 1;
+        constexpr int kRightMotorPort = 2;
+    }
+    namespace drivetrain
+    {
+        constexpr int kLeftFrontMotorPort = 0;
+        constexpr int kLeftMiddleMotorPort = 0;
+        constexpr int kLeftRearMotorPort = 0;
+        constexpr int kRightFrontMotorPort = 0;
+        constexpr int kRightMiddleMotorPort = 0;
+        constexpr int kRightRearMotorPort = 0;
+    }
+}
