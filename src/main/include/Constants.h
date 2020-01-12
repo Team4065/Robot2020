577aa2f0cc89f45b213fbf388fd584b3e3068512
Hunter Goram <huntergoram2@gmail.com>
2020-01-12 10:51:56 -0500

    Added constants for intake and updated others. -Hunter

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -17,13 +17,30 @@ namespace constants
     }
     namespace drivetrain
     {
-        constexpr int kLeftFrontMotorPort = 3;
-        constexpr int kLeftMiddleMotorPort = 4;
-        constexpr int kLeftRearMotorPort = 5;
-        constexpr int kRightFrontMotorPort = 6;
-        constexpr int kRightMiddleMotorPort = 7;
-        constexpr int kRightRearMotorPort = 8;
+        constexpr int kLeftFrontMotorPort = 3; // Inaccurate!
+        constexpr int kLeftMiddleMotorPort = 4; // Inaccurate!
+        constexpr int kLeftRearMotorPort = 5; // Inaccurate!
+        constexpr int kRightFrontMotorPort = 6; // Inaccurate!
+        constexpr int kRightMiddleMotorPort = 7; // Inaccurate!
+        constexpr int kRightRearMotorPort = 8; // Inaccurate!
         constexpr units::current::ampere_t kMaxCurrentDraw { 30.0 };
         constexpr units::inch_t kWheelDiameter { 6.0 };
+        constexpr units::inch_t kWheelBase { 28.0 }; // Maybe correct
+    }
+    namespace intake
+    {
+        constexpr int kIntakeMotorPort = 0; // Inaccurate!
+        constexpr int kLeftSolenoidPorts[2] = { 0, 0 }; // Inaccurate!
+        constexpr int kRightSolenoidPorts[2] = { 0, 0 }; // Inaccurate!
+        constexpr units::current::ampere_t kMaxCurrentDraw { 25.0 };
+        constexpr float kMotorOperatingPercentage = 0.8f; // 
+    }
+    namespace serializer
+    {
+
+    }
+    namespace control_panel_manipulator
+    {
+        
     }
 }
