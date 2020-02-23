5e64fb826d78ccbc55a21de23983aa9edbe98cd3
Romell2018 <mertisromell@icloud.com>
2020-02-22 19:06:44 -0500

    update

--- src/main/include/RobotContainer.h
+++ src/main/include/RobotContainer.h
@@ -4,14 +4,6 @@
 #include <frc/XboxController.h>
 #include <frc/smartdashboard/SendableChooser.h>
 
-#include "subsystems/Shooter.h"
-#include "subsystems/Serializer.h"
-#include "subsystems/Intake.h"
-#include "subsystems/Drivetrain.h"
-#include "subsystems/ControlPanelManipulator.h"
-
-#include "commands/drivetrain/TankDrive.h"
-
 #include "Constants.h"
 
 class RobotContainer
@@ -20,12 +12,6 @@ public:
   RobotContainer();
 
 private:
-  Shooter& shooter_ = Shooter::GetInstance();
-  Serializer& serializer_ = Serializer::GetInstance();
-  Intake& intake_ = Intake::GetInstance();
-  Drivetrain& drivetrain_ = Drivetrain::GetInstance();
-  ControlPanelManipulator& control_panel_manipulator_ = ControlPanelManipulator::GetInstance();
-
   frc::XboxController controller_ { constants::oi::kDriverXboxControllerPort };
 
   frc::SendableChooser<frc2::Command*> chooser_; // Give options for autonomous actions
